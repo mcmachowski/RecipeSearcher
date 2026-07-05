72a3e2766c802d1a29c297d043eb891b53f56408
Marcel <marcelnedza2000@gmail.com>
2026-07-05 15:42:52 +0200

    added tests for desktop navigation

---
+++ tests/pages/RecipesPage.ts
@@ -0,0 +1,12 @@
+import { Page, Locator, expect } from "@playwright/test";
+import { Navbar } from "./Navbar";
+
+export class RecipesPage {
+  readonly page: Page;
+  readonly navbar: Navbar;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.navbar = new Navbar(page);
+  }
+}
