206386ac483886d9ed7f1089e3faede341600f0d
Marcel <marcelnedza2000@gmail.com>
2026-07-08 22:01:40 +0200

    fix for globalSetup URL

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
-  globalSetup: require.resolve("./global-setup"),
+  globalSetup: require.resolve("./all_tests/e2e/global-setup"),
   testDir: "./all_tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
