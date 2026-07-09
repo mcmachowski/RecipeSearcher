b521818328fee90b474b144b3a59142417a80340
Marcel <marcelnedza2000@gmail.com>
2026-07-09 22:05:24 +0200

    added test for searching recipes with filters and checking if recipes details matches the filters

--- tests/all_tests/e2e/search.spec.ts
+++ tests/all_tests/e2e/search.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { SearchPage } from "../../pages/SearchPage";
 import { HomePage } from "../../pages/HomePage";
 import { RecipesPage } from "../../pages/RecipesPage";
+import { RecipeDetailPage } from "../../pages/RecipeDetailPage";
 
 test.describe("Search", () => {
   const baseURL = process.env.BASE_URL!;
@@ -23,7 +24,17 @@ test.describe("Search", () => {
     await homePage.navbar.goToSearchPage();
   });
 
-  test("user can search without filters", async ({ page }) => {
+  test("user can see choosen filters in Filter Panel", async ({ page }) => {
+    const searchPage = new SearchPage(page);
+
+    await searchPage.chooseFilters(myFilters);
+    expect(await searchPage.areChoosenFiltersVisible(myFilters)).toBe(true);
+
+    await searchPage.chooseFilters(myFilters2);
+    expect(await searchPage.areChoosenFiltersVisible({ ...myFilters, ...myFilters2 })).toBe(true);
+  });
+
+  test("user can search for a recipe - without filters", async ({ page }) => {
     const searchPage = new SearchPage(page);
     await searchPage.searchForRecipe();
 
@@ -34,16 +45,27 @@ test.describe("Search", () => {
     expect(totalCount).toBeGreaterThan(0);
   });
 
-  test.only("user can see choosen filters in Filter Panel", async ({ page }) => {
+  test("user can search for a recipe - by filters", async ({ page }) => {
     const searchPage = new SearchPage(page);
-
     await searchPage.chooseFilters(myFilters);
     expect(await searchPage.areChoosenFiltersVisible(myFilters)).toBe(true);
 
-    await searchPage.chooseFilters(myFilters2);
-    expect(await searchPage.areChoosenFiltersVisible({ ...myFilters, ...myFilters2 })).toBe(true);
+    await searchPage.searchForRecipe();
+    const searchedRecipesPage = new RecipesPage(page);
+
+    await expect(page).toHaveURL(`${baseURL}/searched-recipes`);
+    const totalCount = await searchedRecipesPage.getTotalRecipesCount();
+    expect(totalCount).toBeGreaterThan(0);
+
+    const myCategory = myFilters.category || myFilters2.category;
+
+    await searchedRecipesPage.goToRecipeDetailPageByIndex(0);
+
+    const recipeDetailPage = new RecipeDetailPage(page);
+    expect(await recipeDetailPage.getRecipeCategory()).toBe(myCategory);
   });
 
-  test.skip("user can search for a recipe - by search input", async ({ page }) => {});
-  test.skip("user can search for a recipe - by search input and filters", async ({ page }) => {});
+  test("user can search for a recipe - by search input", async ({ page }) => {});
+
+  test("user can search for a recipe - by search input and filters", async ({ page }) => {});
 });
--- tests/pages/RecipeDetailPage.ts
+++ tests/pages/RecipeDetailPage.ts
@@ -8,12 +8,27 @@ export class RecipeDetailPage {
   readonly recipeTitle: Locator;
   readonly ingredientsHeading: Locator;
   readonly instructionsHeading: Locator;
+  readonly recipeCategory: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.navbar = new Navbar(page);
+
     this.recipeTitle = page.getByRole("heading", { level: 1 });
-    this.ingredientsHeading = page.getByRole("heading", { name: "Ingredients:" });
-    this.instructionsHeading = page.getByRole("heading", { name: "Instructions:" });
+    this.ingredientsHeading = page.getByRole("heading", {
+      name: "Ingredients:",
+    });
+    this.instructionsHeading = page.getByRole("heading", {
+      name: "Instructions:",
+    });
+
+    this.recipeCategory = page.locator("p").filter({
+      hasText: /^Category:/,
+    });
+  }
+
+  async getRecipeCategory(): Promise<string> {
+    const text = await this.recipeCategory.textContent();
+    return text!.replace("Category:", "").trim();
   }
 }
