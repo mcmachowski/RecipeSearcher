7502921569a99c564ba84aa30464d2869b4d186d
Marcel <marcelnedza2000@gmail.com>
2026-07-17 19:03:11 +0200

    little refactor for navigation tests

--- tests/all_tests/e2e/navigation.spec.ts
+++ tests/all_tests/e2e/navigation.spec.ts
@@ -3,43 +3,35 @@ import { HomePage } from "../../pages/HomePage";
 
 test.describe("Navigation", () => {
   const baseURL = process.env.BASE_URL!;
+  let homePage: HomePage;
 
-  test("user can navigate from Home to Search Page", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
     await homePage.open();
     await expect(page).toHaveURL(baseURL);
+  });
+
+  test("user can navigate from Home to Search Page", async ({ page }) => {
     await homePage.navbar.goToSearchPage();
     await expect(page).toHaveURL(`${baseURL}/search`);
   });
 
   test("user can navigate from Home to Recipes Page", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.open();
-    await expect(page).toHaveURL(baseURL);
     await homePage.navbar.goToRecipesPage();
     await expect(page).toHaveURL(`${baseURL}/recipes`);
   });
 
   test("user can navigate from Home to Sign In Page", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.open();
-    await expect(page).toHaveURL(baseURL);
     await homePage.navbar.goToSignInPage();
     await expect(page).toHaveURL(`${baseURL}/sign-in`);
   });
 
   test("user can navigate from Home to Sign Up Page", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.open();
-    await expect(page).toHaveURL(baseURL);
     await homePage.navbar.goToSignUpPage();
     await expect(page).toHaveURL(`${baseURL}/sign-up`);
   });
 
   test("user can navigate to Search Page and come back to Home Page", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.open();
-    await expect(page).toHaveURL(baseURL);
     await homePage.navbar.goToSearchPage();
     await expect(page).toHaveURL(`${baseURL}/search`);
     await homePage.navbar.goToHomePageByHomeButton();
@@ -47,9 +39,6 @@ test.describe("Navigation", () => {
   });
 
   test("user can navigate to Search Page and come back to Home Page with logo button", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.open();
-    await expect(page).toHaveURL(baseURL);
     await homePage.navbar.goToSearchPage();
     await expect(page).toHaveURL(`${baseURL}/search`);
     await homePage.navbar.goToHomePageByLogo();
