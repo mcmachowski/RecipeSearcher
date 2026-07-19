b3e40c216b08359e96f427af4ae65122a8cdf900
Marcel <marcelnedza2000@gmail.com>
2026-07-19 15:03:19 +0200

    added method chooseAndExpectFiltersToBeVisible; added it in test & added and structed test.step in search.spec.ts

--- tests/all_tests/e2e/search.spec.ts
+++ tests/all_tests/e2e/search.spec.ts
@@ -23,49 +23,89 @@ test.describe("Search", () => {
     await homePage.navbar.goToSearchPage();
   });
 
-  test("user can see choosen filters in Filter Panel", async ({ page }) => {
-    await searchPage.chooseFilters(myFilters);
-    expect(await searchPage.areChoosenFiltersVisible(myFilters)).toBe(true);
-    await searchPage.chooseFilters(myFilters2);
-    expect(await searchPage.areChoosenFiltersVisible({ ...myFilters, ...myFilters2 })).toBe(true);
+  test("user can see choosen filters in Filter Panel", async () => {
+    await searchPage.chooseAndExpectFiltersToBeVisible({ ...myFilters, ...myFilters2 });
   });
 
   test("user can search for a recipe - without filters", async ({ page }) => {
-    await searchPage.searchForRecipe();
-
-    await expect(page).toHaveURL(searchedRecipesUrl);
-    const totalCount = await searchedRecipesPage.getTotalRecipesCount();
-    expect(totalCount).toBeGreaterThan(0);
+    await test.step("user fills recipe's name and presses search button", async () => {
+      await searchPage.searchForRecipe();
+      await expect(page).toHaveURL(searchedRecipesUrl);
+    });
+    await test.step("user sees at least one recipe", async () => {
+      const totalCount = await searchedRecipesPage.getTotalRecipesCount();
+      expect(totalCount).toBeGreaterThan(0);
+    });
   });
 
   test("user can search for a recipe - by filters", async ({ page }) => {
-    await searchPage.chooseFilters(myFilters);
-    expect(await searchPage.areChoosenFiltersVisible(myFilters)).toBe(true);
-    await searchPage.searchForRecipe();
-    await expect(page).toHaveURL(searchedRecipesUrl);
-    const totalCount = await searchedRecipesPage.getTotalRecipesCount();
-    expect(totalCount).toBeGreaterThan(0);
-    await searchedRecipesPage.goToRecipeDetailPageByIndex(0);
     const recipeDetailPage = new RecipeDetailPage(page);
-    expect(await recipeDetailPage.getRecipeCategory()).toBe(myCategory);
+
+    await test.step("User fills filters", async () => {
+      await searchPage.chooseAndExpectFiltersToBeVisible(myFilters);
+    });
+
+    await test.step("User searches for a recipe", async () => {
+      await searchPage.searchForRecipe();
+    });
+
+    await test.step("User checks whether the page redirects to the new URL /searched-recipes", async () => {
+      await expect(page).toHaveURL(searchedRecipesUrl);
+    });
+
+    await test.step("User checks if there are any recipes visible", async () => {
+      const totalCount = await searchedRecipesPage.getTotalRecipesCount();
+      expect(totalCount).toBeGreaterThan(0);
+    });
+
+    await test.step("User goes to details of the specified recipe", async () => {
+      await searchedRecipesPage.goToRecipeDetailPageByIndex(0);
+    });
+
+    await test.step("User sees category is correct", async () => {
+      expect(await recipeDetailPage.getRecipeCategory()).toBe(myCategory);
+    });
   });
 
   test("user can search for a recipe - by search input", async ({ page }) => {
-    await searchPage.searchForRecipe(recipeDataForTests.recipeNameToSearch);
-    await expect(page).toHaveURL(searchedRecipesUrl);
-    const totalCount = await searchedRecipesPage.getTotalRecipesCount();
-    expect(totalCount).toBeGreaterThan(0);
+    await test.step("User searches for a recipe with input name", async () => {
+      await searchPage.searchForRecipe(recipeDataForTests.recipeNameToSearch);
+    });
+
+    await test.step("User checks whether the page redirects to the new URL /searched-recipes", async () => {
+      await expect(page).toHaveURL(searchedRecipesUrl);
+    });
+
+    await test.step("User checks if there are any recipes visible", async () => {
+      const totalCount = await searchedRecipesPage.getTotalRecipesCount();
+      expect(totalCount).toBeGreaterThan(0);
+    });
+
     expect(await searchedRecipesPage.recipeNameHeadings.first().textContent()).toContain(recipeDataForTests.recipeNameToSearch);
   });
 
   test("user can search for a recipe - by search input and filters", async ({ page }) => {
-    await searchPage.chooseFilters(myFilters);
-    expect(await searchPage.areChoosenFiltersVisible(myFilters)).toBe(true);
-    await searchPage.searchForRecipe(recipeDataForTests.recipeNameToSearch);
-    await expect(page).toHaveURL(searchedRecipesUrl);
-    const totalCount = await searchedRecipesPage.getTotalRecipesCount();
-    expect(totalCount).toBeGreaterThan(0);
-    expect(await searchedRecipesPage.recipeNameHeadings.first().textContent()).toContain(recipeDataForTests.recipeNameToSearch);
+    await test.step("User fills filters", async () => {
+      await searchPage.chooseAndExpectFiltersToBeVisible(myFilters);
+    });
+
+    await test.step("User searches for a recipe with input name", async () => {
+      await searchPage.searchForRecipe(recipeDataForTests.recipeNameToSearch);
+    });
+
+    await test.step("User checks whether the page redirects to the new URL /searched-recipes", async () => {
+      await expect(page).toHaveURL(searchedRecipesUrl);
+    });
+
+    await test.step("User checks if there are any recipes visible", async () => {
+      const totalCount = await searchedRecipesPage.getTotalRecipesCount();
+      expect(totalCount).toBeGreaterThan(0);
+    });
+
+    await test.step("User can see that title of the searched recipe is correct", async () => {
+      expect(await searchedRecipesPage.recipeNameHeadings.first().textContent()).toContain(recipeDataForTests.recipeNameToSearch);
+    });
+
     await searchedRecipesPage.goToRecipeDetailPageByIndex(0);
     const recipeDetailPage = new RecipeDetailPage(page);
     expect(await recipeDetailPage.getRecipeCategory()).toBe(myCategory);
